44881aba783e61513e7ce82413359d864bd89a1f
ersiko <tomas@criptos.com>
2016-02-05 14:13:44 +0100

    New version with puppetforge files

--- lib/facter/facts.rb
+++ lib/facter/facts.rb
@@ -6,3 +6,15 @@ Facter.add('role') do
     role
   end
 end
+
+Facter.add("env") do
+  setcode do
+    Facter.value('fqdn').split('.')[-3]
+  end
+end
+
+Facter.add("datacenter") do
+  setcode do
+    Facter.value('fqdn').split('.')[-4]
+  end
+end
